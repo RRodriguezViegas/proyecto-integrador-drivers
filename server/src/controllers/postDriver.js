d22ba7ee2489dcd47c77f85787ad9447c2e96bad
RRodriguezViegas <ramiirv26@gmail.com>
2023-11-24 20:37:02 -0300

    vamos a ver

--- server/src/controllers/postDriver.js
+++ server/src/controllers/postDriver.js
@@ -8,67 +8,25 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-// const cloudinary = require("cloudinary").v2;
-// const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
-//   process.env;
-// cloudinary.config({
-//   cloud_name: CLOUDINARY_CLOUD_NAME,
-//   api_key: CLOUDINARY_API_KEY,
-//   api_secret: CLOUDINARY_API_SECRET,
-//   secure: true,
-// });
-
-// // upload image function
-// const uploadImage = async imagePath => {
-//   const options = {
-//     use_filename: true,
-//     unique_filename: false,
-//     overwrite: true,
-//   };
-
-//   try {
-//     const result = await cloudinary.uploader.upload(imagePath, options);
-//     // console.log(result);
-//     return result.secure_url;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// let imageName = "";
-
-// const storage = multer.diskStorage({
-//   destination: path.join("./image"),
-//   filename: function (req, file, cb) {
-//     imageName = Date.now() + path.extname(file.originalname);
-//     cb(null, imageName);
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 3000000 },
-// }).single("myImage");
 
 const postDriver = async (req, res) => {
   // try {
-  // try {
-  //   if (req.file) {
-  //     // const result = await cloudinary.uploader.upload(req.image.path);
-  //     console.log(result);
-  //     res.status(201).json({
-  //       message: "nice",
-  //     });
-  //   } else {
-  //     res.status(404).send({ status: "error", message: `File not found!` });
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(500).send({ status: "err", error: err });
-  // }
+  try {
+    if (req) {
+      console.log(req);
+      // const result = await cloudinary.uploader.upload(req.image.path);
+      res.status(201).json({ status: "success" });
+    } else {
+      res.status(404).send({ status: "error", message: `File not found!` });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ status: "err", error: err });
+  }
   // if (!name || !surname || !nationality || !dob) {
   //   return res.status(400).json({ error: "Faltan datos obligatorios" });
   // }
+
   // upload(image, res, err => {
   //   if (err) {
   //     console.log(err);
@@ -78,10 +36,12 @@ const postDriver = async (req, res) => {
   //     });
   //   }
   // });
+
   // if (image) {
   //   const result = await uploadImage(image);
   //   image = result;
   // }
+
   // const newDriver = await Driver.create({
   //   name,
   //   surname,
@@ -90,6 +50,7 @@ const postDriver = async (req, res) => {
   //   nationality,
   //   dob,
   // });
+
   // const teamObjects = await Team.findAll({
   //   where: {
   //     nombre: {
@@ -97,7 +58,9 @@ const postDriver = async (req, res) => {
   //     },
   //   },
   // });
+
   // await newDriver.setTeams(teamObjects);
+
   //   res.status(201).json({ message: "OwO" });
   // } catch (error) {
   //   res.status(400).json({ error: error.message });
